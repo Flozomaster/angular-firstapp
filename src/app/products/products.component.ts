6adacb5b36978a6ddfd73cb8a09e661f613a8866
Flozomaster <flozot7@gmail.com>
2025-01-05 15:48:22 +0000

    Add List of Products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+    products! : Array<any>;
+
+    constructor() {
+
+    }
+
+    ngOnInit(): void {
+
+        this.products = [
+            {id: 1, name: "Computer", price: "6500"},
+            {id: 2, name: "Printer", price: "1200"},
+            {id: 3, name: "Smart phone", price: "6500"},
+            {id: 4, name: "Mouse", price: "200"}
+        ];
+
+    }
 
 }
